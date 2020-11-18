ae292460f4eedf20749ee3f292f98ca8bfa8102a
Prathibha Ratnayake <prsthibha@gmail.com>
2020-11-18 18:18:13 +0530

    mongodb connected

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.get("/api/products", (req, res) => {
   res.json(products);
 });
 
 app.get("/api/products/:id", (req, res) => {
-  const product = find((p) => p._id === req.params.id);
+  const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
 
