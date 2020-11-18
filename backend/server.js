b164c61b71a6b3e93500a27ef1b610fa3262ec9e
Prathibha Ratnayake <prsthibha@gmail.com>
2020-11-18 16:12:26 +0530

    env variable setup

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const products = require("./data/products.js");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
 app.get("/api/products", (req, res) => {
   res.json(products);
 });
 
 app.get("/api/products/:id", (req, res) => {
-    const product = products.find(p=>p._id === req.params.id)
-    res.json(product);
-  });
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
-app.listen(5000, () => console.log(`server listening om port 5000`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`server listening in ${process.env.NODE_ENV} port ${PORT}`),
+);
