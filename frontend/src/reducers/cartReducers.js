d1a5e96edde4f4146ca78e82873b9002d6fb87b5
Prathibha Ratnayake <prsthibha@gmail.com>
2020-11-20 20:22:13 +0530

    remove items from cart fuctionality added

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   const { type, payload } = action;
@@ -12,7 +12,7 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           ...state,
           cartItems: state.cartItems.map((x) =>
             x.product === existItem.product ? item : x,
-          ), 
+          ),
         };
       } else {
         return {
@@ -20,6 +20,11 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           cartItems: [...state.cartItems, item],
         };
       }
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((x) => x.product !== payload),
+      };
 
     default:
       return state;
