dc4752d5a83cee5e5eacc1890229b25fecb6c5d5
Prathibha Ratnayake <prsthibha@gmail.com>
2020-11-20 13:07:31 +0530

    productDetails reducer & action created

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,7 @@
 import {
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
@@ -18,6 +21,32 @@ export const productListReducer = (state = { products: [] }, action) => {
         products: payload,
       };
     case PRODUCT_LIST_FAIL:
+      return {
+        loading: false,
+        error: payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const productDetailsReducer = (
+  state = { product: { reviews: [] } },
+  action,
+) => {
+  const { type, payload } = action;
+  switch (type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return {
+        loading: true,
+        ...state,
+      };
+    case PRODUCT_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        product: payload,
+      };
+    case PRODUCT_DETAILS_FAIL:
       return {
         loading: false,
         error: payload,
