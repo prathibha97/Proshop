d4c13b6e355f90876c04d237d9c66297d40a7f03
Prathibha Ratnayake <prsthibha@gmail.com>
2020-11-20 10:59:31 +0530

    fetch products from db

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
-import colors from 'colors';
+import colors from "colors";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
-
+import productRoutes from "./routes/productRoutes.js";
 const app = express();
 dotenv.config();
 connectDB();
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
-  console.log(`server listening in ${process.env.NODE_ENV} port ${PORT}`.yellow.bold),
+  console.log(
+    `server listening in ${process.env.NODE_ENV} port ${PORT}`.yellow.bold,
+  ),
 );
