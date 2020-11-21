da88ed716b9049839630ad2c7d60ca241f9e2a0c
Prathibha Ratnayake <prsthibha@gmail.com>
2020-11-21 18:03:17 +0530

    user register action,reducer & screen added

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -3,6 +3,9 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = { products: [] }, action) => {
@@ -24,6 +27,29 @@ export const userLoginReducer = (state = { products: [] }, action) => {
       };
     case USER_LOGOUT:
       return {};
+    default:
+      return state;
+  }
+};
+
+export const userRegisterReducer = (state = {}, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case USER_REGISTER_REQUEST:
+      return {
+        loading: true,
+      };
+    case USER_REGISTER_SUCCESS:
+      return {
+        loading: false,
+        userInfo: payload,
+      };
+    case USER_REGISTER_FAIL:
+      return {
+        loading: false,
+        error: payload,
+      };
+
     default:
       return state;
   }
