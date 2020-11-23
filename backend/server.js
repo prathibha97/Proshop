e516ac599bad0e7709f21e1bf96190854385ece1
Prathibha Ratnayake <prsthibha@gmail.com>
2020-11-23 09:25:23 +0530

    order controller & route added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -14,6 +15,7 @@ connectDB();
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use(notFound);
 
