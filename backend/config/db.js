5c5f2d40c93ac7e39b44b2d2da44c42ad786fe38
Prathibha Ratnayake <60864062+prathibha97@users.noreply.github.com>
2020-11-27 14:07:59 +0530

    mongo atlas connected

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost/proshop', {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true,
